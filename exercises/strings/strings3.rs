f0349c1337412b1d7940cecf103a43f975594db8
Xlawy <20221000638@cug.edu.cn>
2025-04-07 08:37:29 +0800

    完成了一些题目

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,35 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut ptr1 = 0;
+    let mut ptr2 = input.len();
+
+    for (i, c) in input.char_indices() {
+        if c != ' ' {
+            ptr1 = i;
+            break;
+        }
+    }
+
+    for (i, c) in input.char_indices().rev() {
+        if c != ' ' {
+            ptr2 = i;
+            break;
+        }
+    }
+
+    input[ptr1..=ptr2].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    input.to_string() + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -39,7 +53,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
